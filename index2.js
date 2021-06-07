2188198d35d3bdb20aa34b7f8fd7ef3899cc7898
Đường Trung Anh <trung.anh.duong.1995@gmail.com>
2021-06-07 20:11:49 +0700

    Add files via upload
    Initialization commit

---
+++ index2.js
@@ -0,0 +1,43 @@
+// 5.3
+const products = [
+    {
+        name: 'Xiaomi portable charger 20000mah',
+        brand: 'Xiaomi',
+        price: 428,
+        color: 'White',
+        category: 'Charger'
+    }, {
+        name: 'VSmart Active 1',
+        brand: 'VSmart',
+        price: 5487,
+        color: 'Black',
+        category: 'Phone'
+    }, {
+        name: 'IPhone X',
+        brand: 'Apple',
+        price: 21490,
+        color: 'Gray',
+        category: 'Phone'
+    }, {
+        name: 'Samsung Galaxy A9',
+        brand: 'Samsung',
+        price: 8490,
+        color: 'Blue',
+        category: 'Phone',
+        providers: []
+    },
+];
+
+// get userInput
+// loop through all products 
+// with each product, read the category property
+// if this product's category = userInput => console.log this product ==
+
+
+// get providerName
+// loop through all products 
+// with each product, read provider array
+// if this product's provider array includes providerName=> console.log this product
+// providers.includes(providerName) => boolean => true/false
+// providers.indexOf(providerName) => -1
+
